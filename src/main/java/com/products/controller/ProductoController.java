7760c82e5b15d4a89a167088debd31f01ab976c4
Carlos Oliveros <carlosandmain@gmail.co>
2025-11-22 19:23:47 -0500

    US-004 Actualización de Stock

--- src/main/java/com/products/controller/ProductoController.java
+++ src/main/java/com/products/controller/ProductoController.java
@@ -6,11 +6,8 @@ import com.products.service.ProductosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -28,6 +25,12 @@ public class ProductoController {
     public ResponseEntity<List<Producto>> listar(){
         return new ResponseEntity<>(productoService.listar(), HttpStatus.OK);
     }
+
+    @PutMapping("/{id}/stock")
+    public ResponseEntity<Producto>actualizarStock(@PathVariable Long id, @RequestBody Integer nuevoStock){
+        return new ResponseEntity<>(productoService.actualizarStock(id, nuevoStock),HttpStatus.OK);
+    }
+
 }
 
 
